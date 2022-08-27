05e3bca8735aea84987e0518f5f382d095745873
Ludwig Hubert <ludwig.hubert@gmail.com>
2022-08-27 08:07:10 +0200

    Add basic hello world cloud function (tests not running)

---
+++ backend/README.md
@@ -0,0 +1,15 @@
+# Cloud Functions
+
+## Install depenendcies
+
+```shell
+pip3 install -r requirements.txt
+pip3 install -r requirements-test.txt
+```
+
+## Run function locally
+
+```shell
+functions-framework --target=hello_get
+functions-framework --target=hello_get --debug
+```
---
+++ backend/main.py
@@ -0,0 +1,58 @@
+import sys
+
+# [START functions_helloworld_http]
+# [START functions_http_content]
+from flask import escape
+# [START functions_http_method]
+# [START functions_helloworld_get]
+import functions_framework
+
+# [END functions_helloworld_http]
+# [END functions_http_content]
+# [END functions_http_method]
+# [END functions_helloworld_get]
+
+
+# [START functions_helloworld_get]
+@functions_framework.http
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    return 'Hello World!'
+# [END functions_helloworld_get]
+
+
+# [START functions_helloworld_http]
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
+# [END functions_helloworld_http]
